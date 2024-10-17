34bd29803b585a16f3a004174c1529f3d730f14e
SKKUGoon <baesangil0906@gmail.com>
2024-10-17 10:33:52 +0900

    test: prefect

---
+++ weather/weather_etl.py
@@ -0,0 +1,38 @@
+from prefect import flow, task
+import os
+
+
+@flow(name="test flow", log_prints=True)
+def test_flow() -> str:
+    test_task1()  # Flow can call tasks
+    test_task2()
+    check_env()  # Check inserted envs
+    my_nested_flow()  # Flow can call nested flows
+    return "Hello, world!"
+
+
+@task
+def test_task1():
+    print("Test Task 1!")
+
+
+@task
+def test_task2():
+    print("Test Task 2!")
+
+
+@task
+def check_env():
+    print("weather api", os.getenv("APIKEY_KOREA_WEATHER"))
+    print("weather api", os.getenv("PG_HOST"))
+
+
+@flow(name="nested flow")
+def my_nested_flow(msg):
+    print(f"Nestedflow says: {msg}")
+
+
+if __name__ == "__main__":
+    test_flow()
+
+
